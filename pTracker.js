9dd10c3ae55c8d8fdb4911266cca1856b3fe4441
ukhan12 <ubkhan3@gmail.com>
2022-02-09 13:47:11 -0500

    Add comments

--- pTracker.js
+++ pTracker.js
@@ -1,3 +1,4 @@
+//Pulls the list of words for the set of letters currently being shown in game  
 function getWordList(letters, obj){
     let words;
     for(prop in obj){
@@ -8,7 +9,8 @@ function getWordList(letters, obj){
     return words;
 };
 const wordList = getWordList();
-
+//Check to make sure the user input matches one of the words matches 
+//in the array for that set of letter
 function isCorrect(userInput, wordList){
     for(let i = 0; i < wordList.length; i++){
         if(userInput === wordList[i]){
