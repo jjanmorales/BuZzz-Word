75f87394e37fab1be413dca01c3434d2e761de1c
ukhan12 <ubkhan3@gmail.com>
2022-02-09 21:48:11 -0500

    Add function to update word count in display message

--- pTracker.js
+++ pTracker.js
@@ -1,3 +1,4 @@
+//Pulls the list of words for the set of letters currently being shown in game  
 function getWordList(letters, obj){
     let words;
     for(prop in obj){
@@ -7,8 +8,9 @@ function getWordList(letters, obj){
     }
     return words;
 };
-const wordList = getWordList();
-
+let wordList = getWordList();
+//Check to make sure the user input matches one of the words matches 
+//in the array for that set of letter
 function isCorrect(userInput, wordList){
     for(let i = 0; i < wordList.length; i++){
         if(userInput === wordList[i]){
@@ -16,4 +18,14 @@ function isCorrect(userInput, wordList){
         }
     }
     return false;
+}
+
+//Update message for when user gets correct word
+let gotWord = isCorrect();  
+let count = 0;
+function updateWordsFoundString(gotWord){
+    if(gotWord){   //Make sure string is not empty
+        count++;
+        console.log(`You have found ${count} words.`)
+    }
 }
