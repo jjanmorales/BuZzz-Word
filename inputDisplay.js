8783baccec62c220807de5645cb920a2f96d884c
ukhan12 <ubkhan3@gmail.com>
2022-02-11 15:39:30 -0500

    change file name

---
+++ inputDisplay.js
@@ -0,0 +1,7 @@
+import {words} from "./wordsObj.js";
+import {enterCurrentWord} from "./main-script.js";
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(words);
+    console.log("hi")
+
+})
--- wordsObj.js
+++ wordsObj.js
@@ -1,4 +1,4 @@
-const words = {
+export const words = {
   GADNRUV: [
     "vanguard",
     "agar",
