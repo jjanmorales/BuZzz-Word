f8d900bf9d9e809aa419d00d2b156e3fe035100d
ukhan12 <ubkhan3@gmail.com>
2022-02-13 14:48:44 -0500

    Add verification for guessed words

--- inputDisplay.js
+++ inputDisplay.js
@@ -2,6 +2,8 @@ import {words} from "./wordsObj.js";
 document.addEventListener("DOMContentLoaded", () => {
     displayLetters();   
     checkUserInput(); 
+    console.log(wordList)
+    
 })
 
 
@@ -15,11 +17,13 @@ const wordsFoundUl = document.createElement("ul");
 wordsFound.appendChild(wordsFoundUl);
 
 //extract groups of letters from words object to display in hexagons
+//Extract word list for that group of letters
 let letterGroups = [];
 for(let prop in words){
     letterGroups.push(prop);
 }
 let currentGameLetters = letterGroups[0]
+const wordList = getWordList(currentGameLetters);
 
 //Create variable for tags using id
 function createElId(id){
@@ -33,31 +37,43 @@ function displayLetters(){
         letterCell[i].innerText = currentGameLetters[i];
         //Handle click event on the letter cells
         letterCell[i].addEventListener("click", () =>{
-            let chosenLetter = letterCell[i].innerText;
-            userGuess.value += `${chosenLetter}`;
+            userGuess.value += `${letterCell[i].innerText}`;
         });
     }
 }
 
 //Take user input and check to see of the word is correct when they click the enter button
 //If it is correct, take the word and display it in the white right-side box and
-//Update the sentence at the top of the box to reflect amount of found words 
+//increment amount of words found in the sentence on top 
 function checkUserInput(){
     enter.addEventListener("click", () =>{
-        if((userGuess.value).length > 3){
+        if((userGuess.value).length > 3 && isCorrect(userGuess.value, wordList)){
          const wordsFoundLi = document.createElement("li");
          wordsFoundLi.innerText = userGuess.value;
          wordsFoundUl.appendChild(wordsFoundLi);
          wordsFoundLi.style.listStyle = 'none';
          counter.innerText = Number(counter.innerText) + Number(1);
          userGuess.value = "";
+       }else{
+        userGuess.value = "";
        }
     })
 }
 
+//Pulls the list of words for the set of letters currently being shown in game  
+function getWordList(letters){
+    for(let prop in words){
+        if(letters === prop){
+          return words[prop];
+        }
+    }
+};
 
-
-
+//Check to make sure the user input matches one of the words in the array for that set of letters
+//Takes in array of words for the current game letters and the word guessed by user
+function isCorrect(userInput, wordList){
+    return wordList.includes(userInput.toLowerCase());
+}
 
 
 
--- pTracker.js
+++ pTracker.js
@@ -1,13 +1,4 @@
-//Pulls the list of words for the set of letters currently being shown in game  
-function getWordList(letters, obj){
-    let words;
-    for(prop in obj){
-        if(letters === prop){
-          words = obj[prop];
-        }
-    }
-    return words;
-};
+
 const wordList = getWordList();
 //Check to make sure the user input matches one of the words matches 
 //in the array for that set of letters
