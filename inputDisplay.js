865d9021e0d7c07af1252416bcb1b220c422e4e6
ukhan12 <ubkhan3@gmail.com>
2022-02-13 13:58:51 -0500

    Organize code and add comments

--- inputDisplay.js
+++ inputDisplay.js
@@ -1,46 +1,59 @@
 import {words} from "./wordsObj.js";
-// import {enterCurrentWord} from "./main-script.js";
 document.addEventListener("DOMContentLoaded", () => {
-   //extract groups of letters from words object to display in hexagons
-    let letterGroups = [];
-    for(let prop in words){
-     letterGroups.push(prop);
-     }
-    let currentGameLetters = letterGroups[0]
-    const userGuess = document.getElementById("user-guess");
-    const letterCell = document.getElementsByClassName("letter-cell");
-    const enter = document.getElementById("enter-btn");
-    const wordsFound = document.getElementById("words-found");
-    const wordsFoundUl = document.createElement("ul");
-    const counter = document.getElementById("counter");
-    wordsFound.appendChild(wordsFoundUl);
+    displayLetters();   
+    checkUserInput(); 
+})
+
+
+const userGuess = createElId("user-guess");
+const enter = createElId("enter-btn");
+const wordsFound = createElId("words-found");
+const counter = createElId("counter");
+const letterCell = document.getElementsByClassName("letter-cell");
+//Create list for words that are create and append to white box
+const wordsFoundUl = document.createElement("ul");
+wordsFound.appendChild(wordsFoundUl);
+
+//extract groups of letters from words object to display in hexagons
+let letterGroups = [];
+for(let prop in words){
+    letterGroups.push(prop);
+}
+let currentGameLetters = letterGroups[0]
+
+//Create variable for tags using id
+function createElId(id){
+    return document.getElementById(id);
+}
+
+//Display letters that are clicked on in display cell
+function displayLetters(){
     for(let i = 0; i < letterCell.length; i++){
+        //Display letters on the cells
         letterCell[i].innerText = currentGameLetters[i];
+        //Handle click event on the letter cells
         letterCell[i].addEventListener("click", () =>{
             let chosenLetter = letterCell[i].innerText;
             userGuess.value += `${chosenLetter}`;
         });
     }
+}
+
+//Take user input and check to see of the word is correct when they click the enter button
+//If it is correct, take the word and display it in the white right-side box and
+//Update the sentence at the top of the box to reflect amount of found words 
+function checkUserInput(){
     enter.addEventListener("click", () =>{
         if((userGuess.value).length > 3){
-            const wordsFoundLi = document.createElement("li");
-            wordsFoundLi.innerText = userGuess.value;
-            wordsFoundUl.appendChild(wordsFoundLi);
-            wordsFoundLi.style.listStyle = 'none';
-            counter.innerText = Number(counter.innerText) + Number(1);
-            userGuess.value = "";
-        }
-        
+         const wordsFoundLi = document.createElement("li");
+         wordsFoundLi.innerText = userGuess.value;
+         wordsFoundUl.appendChild(wordsFoundLi);
+         wordsFoundLi.style.listStyle = 'none';
+         counter.innerText = Number(counter.innerText) + Number(1);
+         userGuess.value = "";
+       }
     })
-     //for loop closing bracket targeting each cell 
-  
-    // enter.addEventListener("click", enterCurrentWord());
-
-    
-    
-})
-
-
+}
 
 
 
